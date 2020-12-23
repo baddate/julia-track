ff605a788e7789e33e3375c308c639ac1161de7e
baddate <badgatex@outlook.com>
2020-12-23 19:45:25 +0800

    modified: add decipher function

--- rotational-cipher/rotational-cipher.jl
+++ rotational-cipher/rotational-cipher.jl
@@ -20,6 +20,27 @@ function rotate(shift::Int64, plain)
 end
 
 """
+decipher function
+"""
+function decipher_rotate(shift, cipher)
+    table = "abcdefghijklmnopqrstuvwxyz"
+    plain = ""
+    if typeof(cipher) == Char
+        pos = findfirst(cipher, table)-shift > 0 ? findfirst(cipher, table)-shift : findfirst(cipher, table)-shift+26
+        return table[pos]
+    end
+    for c in cipher
+        if findfirst(c, table) === nothing && findfirst(c, uppercase(table)) === nothing
+            cipher *= c
+            continue
+        end
+        pos = findfirst(lowercase(c), table)-shift > 0 ? findfirst(lowercase(c), table)-shift : findfirst(lowercase(c), table)-shift+26
+        plain *= isuppercase(c) ? uppercase(table[pos]) : table[pos]
+    end
+    return plain
+end
+
+"""
 Bonus A
 @R5_str str
 @R5_str(str)
